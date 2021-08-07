1d34b9a2d4543a7037c1c66f1c7b9c34abef9eb9
Volker Sedlmair <volkersedlmair@web.de>
2021-08-07 13:15:56 +0200

    wired voice control to button

--- src/app/modules/pathway/components/pathway-control/pathway-control.component.ts
+++ src/app/modules/pathway/components/pathway-control/pathway-control.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faCoffee, faMicrophone, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { SpeechRecognitionService } from 'src/app/shared/services/speech/speech-recognition.service';
+import { PathwayEvent } from '../../model/PathwayEvent';
 
 @Component({
   selector: 'pathway-control',
@@ -10,13 +12,23 @@ import { faCoffee, faMicrophone, IconDefinition } from '@fortawesome/free-solid-
 })
 export class PathwayControlComponent implements OnInit {
 
-  @Input() speechRecognitionAvailable: boolean = false;
+  public speechRecognitionAvailable: boolean = false;
+
+  @Output() pathwayEventEmitter = new EventEmitter<PathwayEvent>();
 
   voiceControlIcon: IconDefinition = faMicrophone;
 
-  constructor(private matSnackbarService: MatSnackBar) { }
+  constructor(private matSnackbarService: MatSnackBar, private speechRecognitionService: SpeechRecognitionService) { }
 
   ngOnInit(): void {
+
+    this.speechRecognitionAvailable = this.speechRecognitionService.init();
+
+    if (!this.speechRecognitionAvailable) {
+      console.warn("Speech recognition not available. Please use Google Chrome or Microsoft Edge instead.")
+    } else {
+      this.setupSpeechRecognitionEventListeners();
+    }
 
     if (!this.speechRecognitionAvailable) {
 
@@ -31,10 +43,62 @@ export class PathwayControlComponent implements OnInit {
 
   }
 
-  onVoiceRecognitionActivated() {
+  private setupSpeechRecognitionEventListeners(): void {
 
-    
+    // when the recognition was successful
+    this.speechRecognitionService.speechRecognition.addEventListener('result',(e)=>{
 
-  }
+      // get the first alternative from the first result and transcibe it (print what was recognized)
+      let transcript: string = e.results[0][0].transcript;
+
+      transcript = transcript.toLowerCase();
+
+      console.log(transcript);
+
+      // since grammars don't work yet, we need to to a string check on the said words
+      if (transcript === "neues element") {
+
+        // create a new pathway event
+        let newPathwayEvent : PathwayEvent = {
+          content: "Ich bin ein durch Sprache erstellter Termin",
+          header: "Neuer Speech Recognition Termin",
+          date: new Date()
+        };
+
+        this.pathwayEventEmitter.emit(newPathwayEvent);
+        console.log("Created a new date in the pathway");
+
+      }
+      
+      
+
+    })
+
+    // when the user stopped talking
+    this.speechRecognitionService.speechRecognition.addEventListener("speechend",(e)=>{
+
+      console.log("Speech has ended!");
+
+      this.speechRecognitionService.speechRecognition.stop();
+
+    })
+
+    // when there was no match during speech recognition
+    this.speechRecognitionService.speechRecognition.addEventListener("nomatch",(e)=> {
+      console.log("We couldn't recognize what you said!");
+    })
+
+    // when an unexpected error occured
+    this.speechRecognitionService.speechRecognition.addEventListener("error",(e)=> {
+      console.error("An error occured during speech recognition: " + e.error);
+    })
+
+}
+
+public onActivateVoiceControl(): void {
+
+  this.speechRecognitionService.startRecognition();
+
+}
 
 }
--- src/app/modules/pathway/components/pathway-view/pathway-view.component.ts
+++ src/app/modules/pathway/components/pathway-view/pathway-view.component.ts
@@ -10,24 +10,11 @@ import { PathwayEvent } from '../../model/PathwayEvent';
 })
 export class PathwayViewComponent implements OnInit {
 
-  public speechRecognitionReady: boolean = false;
-
   public pathwayEvents: PathwayEvent[] = [];
 
-  constructor(private pathwayService: PathwayService, private speechRecognitionService: SpeechRecognitionService) { }
+  constructor(private pathwayService: PathwayService) { }
 
   ngOnInit(): void {
-
-    this.speechRecognitionReady = this.speechRecognitionService.init();
-
-    if (!this.speechRecognitionReady) {
-      console.warn("Speech recognition not available. Please use Google Chrome or Microsoft Edge instead.")
-    } else {
-      this.setupSpeechRecognitionEventListeners();
-
-      // TODO: wire this to the button in the control panel
-      this.speechRecognitionService.startRecognition();
-    }
 
     // call the business layer and get available events of pathway
     this.pathwayService.getPathwayEntries().subscribe({
@@ -42,52 +29,9 @@ export class PathwayViewComponent implements OnInit {
 
   }
 
-  private setupSpeechRecognitionEventListeners(): void {
+  public onNewPathwayEvent(newPahtwayEvent: PathwayEvent): void {
 
-      // when the recognition was successful
-      this.speechRecognitionService.speechRecognition.addEventListener('result',(e)=>{
-
-        // get the first alternative from the first result and transcibe it (print what was recognized)
-        let transcript: string = e.results[0][0].transcript;
-
-        transcript = transcript.toLowerCase();
-
-        console.log(transcript);
-
-        if (transcript === "neues element") {
-
-          this.pathwayEvents.push({
-            date: new Date(),
-            content: "Test",
-            header: "Test"
-          })
-
-        }
-
-        console.log(this.pathwayEvents);
-        
-        console.log("Created a new date in the pathway");
-
-      })
-
-      // when the user stopped talking
-      this.speechRecognitionService.speechRecognition.addEventListener("speechend",(e)=>{
-
-        console.log("Speech has ended!");
-
-        this.speechRecognitionService.speechRecognition.stop();
-
-      })
-
-      // when there was no match during speech recognition
-      this.speechRecognitionService.speechRecognition.addEventListener("nomatch",(e)=> {
-        console.log("We couldn't recognize what you said!");
-      })
-
-      // when an unexpected error occured
-      this.speechRecognitionService.speechRecognition.addEventListener("error",(e)=> {
-        console.error("An error occured during speech recognition: " + e.error);
-      })
+    this.pathwayEvents.push(newPahtwayEvent);
 
   }
 
